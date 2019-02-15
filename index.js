66eb7e93155c4cc9fbf8e419f8e73602e66bea8e
Lenna <lenna@lennamau.com>
2019-02-15 10:22:28 -0700

    restarting because of error

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+const server = require('./server')
+
+const port = 4000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port}***\n`);
+}); 
